7929e85c6ed5fe3bccdff576d409447da524250b
rr777-crt <ilia2013svetlo@gmail.com>
2025-01-17 16:05:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -105,8 +105,8 @@ function getClickAddGold(q, priceGold) {
 function getScoreForGold(score_n, gold_q) {
     if (score < gold_q) return
 
-    getScore(score_n)
-    getgold(-gold_q)
+    getScore(-gold)
+    getgold(score_n)
 }
 function checkBGImage() {
 
